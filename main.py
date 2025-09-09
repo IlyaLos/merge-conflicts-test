de7d0cb3e5e7c64437eb06149a739ed9cf6e8789
Ilia Los <ilia.los@jetbrains.com>
2025-09-09 14:08:08 +0200

    Remove pre-processing

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import json
 
 from model import run_model
 from postprocessing import run_data_postprocessing
-from preprocessing import run_data_preprocessing
 
 
 def read_data() -> list:
@@ -14,7 +13,6 @@ def read_data() -> list:
 def main():
     print("Starting data pipeline")
     data = read_data()
-    data = run_data_preprocessing(data)
     data = run_model(data)
     data = run_data_postprocessing(data)
     return data
